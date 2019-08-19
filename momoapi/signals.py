edc04f9f39dcf1632583522f0af9f70fc8bb217d
Wasswa Samuel <latimerscope@gmail.com>
2019-08-19 04:44:39 +0200

    Add Signal to trigger API call

---
+++ momoapi/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from momoapi.models import MomoRequest
+from mtnmomo.collection import Collection
+
+client = Collection({
+    "COLLECTION_USER_ID": settings.COLLECTION_USER_ID,
+    "COLLECTION_API_SECRET": settings.COLLECTION_API_SECRET,
+    "COLLECTION_PRIMARY_KEY": settings.COLLECTION_PRIMARY_KEY,
+})
+
+
+@receiver(signals.pre_save, sender=MomoRequest)
+def create_request(sender, instance, created, **kwargs):
+    request = client.requestToPay(
+           mobile="256772123456", amount="600", external_id="123456789", payee_note="dd", payer_message="dd", currency="EUR")
+    return request
